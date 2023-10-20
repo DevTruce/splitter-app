42154a5c5314593e6750d547c41f72c81d661ee3
DevTruce <DevTruce@Gmail.com>
2023-10-19 20:21:30 -0400

    fixed elements

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,15 +2,19 @@
 
 /////////////////////////////////////////////////
 //// ELEMENTS
-const inputBill = document.getElementById("billInput");
-const inputNumberOfPeople = document.getElementById("numberOfPeopleInput");
-const btn5Percent = document.getElementById("tipPercentButton5P");
-const btn10Percent = document.getElementById("tipPercentButton10P");
-const btn15Percent = document.getElementById("tipPercentButton15P");
-const btn25Percent = document.getElementById("tipPercentButton25P");
-const btn50Percent = document.getElementById("tipPercentButton50P");
-const btnCustomPercent = document.getElementById("tipPercentButtonCustomP");
-const btnRest = document.getElementById("resetButton");
+const inputBill = document.querySelector(".billInput");
+const inputNumberOfPeople = document.querySelector(".numberOfPeopleInput");
+const btn5Percent = document.querySelector(".tipPercentButton5P");
+const btn10Percent = document.querySelector(".tipPercentButton10P");
+const btn15Percent = document.querySelector(".tipPercentButton15P");
+const btn25Percent = document.querySelector(".tipPercentButton25P");
+const btn50Percent = document.querySelector(".tipPercentButton50P");
+const btnCustomPercent = document.querySelector(".tipPercentButtonCustomP");
+const btnRest = document.querySelector(".resetButton");
 
-const displayTipAmount = document.getElementById("totalTip");
-const displayTotalAmount = document.getElementById("totalAmount");
+const displayTipAmount = document.querySelector(".totalTip");
+const displayTotalAmount = document.querySelector(".totalAmount");
+
+inputBill.addEventListener("click", function () {
+  console.log(`TEST`);
+});
