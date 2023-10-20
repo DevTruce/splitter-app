577f73c3a85f745dca181e1513b1822b2a13f53f
DevTruce <DevTruce@Gmail.com>
2023-10-19 20:08:47 -0400

    stored elements in js

---
+++ src/javascript/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/////////////////////////////////////////////////
+//// ELEMENTS
+const inputBill = document.getElementById("billInput");
+const inputNumberOfPeople = document.getElementById("numberOfPeopleInput");
+const btn5Percent = document.getElementById("tipPercentButton5P");
+const btn10Percent = document.getElementById("tipPercentButton10P");
+const btn15Percent = document.getElementById("tipPercentButton15P");
+const btn25Percent = document.getElementById("tipPercentButton25P");
+const btn50Percent = document.getElementById("tipPercentButton50P");
+const btnCustomPercent = document.getElementById("tipPercentButtonCustomP");
+const btnRest = document.getElementById("resetButton");
