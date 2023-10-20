b8d16968e755a818a0bcba611f96baefe193419d
DevTruce <DevTruce@Gmail.com>
2023-10-19 20:47:51 -0400

    input value storing, tip value storing

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -15,6 +15,59 @@ const btnRest = document.querySelector(".resetButton");
 const displayTipAmount = document.querySelector(".totalTip");
 const displayTotalAmount = document.querySelector(".totalAmount");
 
-inputBill.addEventListener("click", function () {
-  console.log(`TEST`);
-});
+/////////////////////////////////////////////////
+//// VARIABLES
+let billInputValue = 0;
+let tipPercent = 0;
+
+/////////////////////////////////////////////////
+//// EVENT LISTENERS
+
+const checkBillInput = function () {
+  inputBill.addEventListener("keyup", function (event) {
+    if (
+      event.key === "Enter" ||
+      (event.key === 13 && !isNaN(inputBill.value))
+    ) {
+      billInputValue = Number(inputBill.value);
+
+      console.log(`Bill Input Value: ${typeof billInputValue}`); // DEBUGGING
+      console.log(`Bill Input Value: ${billInputValue}`); // DEBUGGING
+    }
+  });
+};
+
+const checkTipAmount = function () {
+  btn5Percent.addEventListener("click", function (event) {
+    tipPercent = 0.05;
+    console.log(tipPercent);
+  });
+  btn10Percent.addEventListener("click", function (event) {
+    tipPercent = 0.1;
+    console.log(tipPercent);
+  });
+  btn15Percent.addEventListener("click", function (event) {
+    tipPercent = 0.15;
+    console.log(tipPercent);
+  });
+  btn25Percent.addEventListener("click", function (event) {
+    tipPercent = 0.25;
+    console.log(tipPercent);
+  });
+  btn50Percent.addEventListener("click", function (event) {
+    tipPercent = 0.5;
+    console.log(tipPercent);
+  });
+  btnCustomPercent.addEventListener("keyup", function (event) {
+    if (
+      event.key === "Enter" ||
+      (event.key === 13 && !isNaN(btnCustomPercent.value))
+    ) {
+      tipPercent = Number(btnCustomPercent.value);
+      console.log(tipPercent);
+    }
+  });
+};
+
+checkBillInput();
+checkTipAmount();
